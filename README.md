a80993d1b4f33afb50b7d3ead78745b7439ba0d5
tuancoaching <160842913+tuancoaching@users.noreply.github.com>
2024-03-08 13:35:40 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# moneyman
-tea project moneyman
+# 💱 moneyman
+
+![Crates.io](https://img.shields.io/crates/v/moneyman?label=moneyman&style=flat-square)
+![docs.rs](https://img.shields.io/docsrs/moneyman?style=flat-square)
+![Crates.io](https://img.shields.io/crates/v/moneyman_cli?label=moneyman_cli&style=flat-square)
+
+A crusty currency converter
+
+![moneyman_cli](./assets/cli.png)
+
+## Example
+
+```
+$ moneyman convert 50 --from EUR --to PHP --on 2023-05-06 --fallback
+50 EUR -> 3044.5833333333350 PHP on the date 2023-05-06
+```
+
+`moneyman` is also available as a library.
+
+```rust
+use std::path::PathBuf;
+
+use chrono::NaiveDate;
+use rust_decimal_macros::dec;
+use rusty_money::{iso, Money};
+
+fn main() {
+    // Choose where to save the historical data files.
+    let data_dir: PathBuf = dirs::home_dir()
+        .and_then(|home_dir| Some(home_dir.join(".moneyman")))
+        .expect("need a home directory");
+
+    // Fetches the historical data from European Central Bank, and creates an
+    // exchange store.
+    let store = moneyman_core::ExchangeStore::sync(data_dir).expect("failed ze sync");
+
+    let amount_in_usd = Money::from_decimal(dec!(6500), iso::USD);
+    let date = NaiveDate::from_ymd_opt(2023, 5, 4).expect("ok date");
+
+    // Converts 6,500.00 USD to EUR
+    let actual = store.convert_on_date(amount_in_usd, iso::EUR, date);
+    let expected = Money::from_decimal(dec!(5869.6044789597254831135994221), iso::EUR);
+
+    // Shouldn't explode? :)
+    assert_eq!(actual, expected);
+}
+```
+
+## Details
+
+`moneyman` extends on `rusty-money` as it already provides a lot of the things like
+`Money`, `Currency`, `ExchangeRate`, and `Exchange`. However, it does not
+provide any data to actually convert currency. For historical data, `moneyman`
+uses the European Central Bank, and saves its data to its own local data store.
+
+## Caveat
+
+One important thing to know is that `ANY_OTHER_CURRENCY` -> EUR is computed with
+`1/<RATE_OF_ONE_EUR_IN_THAT_OTHER_CURRENCY>` since ECB only contains the
+EUR -> `ANY_OTHER_CURRENCY` rates, not vice versa. This [dividing] is most
+probably not the behavior observed by RealWord™ currency exchanges. So this
+only serves as an approximation at best.
---
+++ flake.nix
@@ -0,0 +1,91 @@
+{
+  inputs = {
+    fenix.url = "github:nix-community/fenix";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, fenix, naersk, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = (import nixpkgs) { inherit system; };
+      fenix' = fenix.packages.${system};
+
+      toolchain = with fenix'; combine [
+        stable.rustc
+        stable.cargo
+        targets.x86_64-unknown-linux-musl.stable.rust-std
+      ];
+
+      naersk' = naersk.lib.${system}.override {
+        cargo = toolchain;
+        rustc = toolchain;
+      };
+
+      pname = "moneyman_cli";
+      version = "0.1.2";
+    in {
+      packages = {
+        x86_64-linux = rec {
+          default = moneyman;
+
+          moneyman = naersk'.buildPackage {
+            inherit pname version;
+
+            src = ./.;
+            doCheck = false;
+            nativeBuildInputs = with pkgs; [ openssl pkg-config ];
+          };
+
+          # FIXME: This is broken atm. I wouldn't use this cause it won't even
+          # compile due to it trying to dynamically link something musl doesn't
+          # apparently have.
+          moneyman-static = naersk'.buildPackage {
+            inherit pname version;
+
+            src = ./.;
+            doCheck = false;
+
+            CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+            CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+
+            nativeBuildInputs = with pkgs; [
+              pkgsStatic.stdenv.cc
+            ];
+
+            buildInputs = [];
+          };
+        };
+      };
+
+      devShells.${system} = {
+        ci = pkgs.mkShell {
+          buildInputs = [
+            fenix'.stable.rustc
+            fenix'.stable.cargo
+            fenix'.stable.clippy
+            fenix'.stable.rustfmt
+          ];
+        };
+
+        default = let
+          rustPackages = with fenix'.stable; [
+            rustc
+            cargo
+            clippy
+            rustfmt
+            pkgs.rust-analyzer
+            pkgs.cargo-flamegraph
+          ];
+
+          nixPackages = with pkgs; [ nil ];
+
+          misc = with pkgs; [
+            pkgsStatic.sqlite
+          ];
+        in pkgs.mkShell {
+          buildInputs = rustPackages ++ nixPackages ++ misc;
+        };
+      };
+    };
+}
